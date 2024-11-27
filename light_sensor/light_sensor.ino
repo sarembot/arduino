779c64b802b24a117e14115b4d577222957a2897
sarembot <sarembot@github.com>
2024-11-27 08:31:59 -0800

    Light sensor circuit using a photoresistor and voltage divider.

---
+++ light_sensor/light_sensor.ino
@@ -0,0 +1,38 @@
+// SIMPLE LIGHT SENSOR - By Mitchell Saremba
+// Uses a photoresistor and voltage divider to detect light, and displays a colour on an RGB LED based on current state of my room light.
+
+const int RED = 11;
+const int GREEN = 10;
+const int BLUE = 9;
+
+const int PHOTORESISTOR = 0;
+int val = 0; // to hold the changing value
+int firstVal;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = analogRead(PHOTORESISTOR);
+  Serial.println(val);
+  delay(500);
+
+  if (val < 100) {
+    digitalWrite(RED, HIGH);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, LOW);
+  } else if (val >= 100 && val < 200) {
+    digitalWrite(RED, LOW);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, HIGH);
+  } else if (val >= 200) {
+    digitalWrite(RED, LOW);
+    digitalWrite(GREEN, HIGH);
+    digitalWrite(BLUE, LOW);
+  }
+}
